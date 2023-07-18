6f1f23aab22d760ba6d84f1bf1e2cef7c9a42425
beatrimt <beatrice.m.trascau@ntnu.no>
2023-07-18 09:29:03 +0200

    This script contains code that combines the CORINE land cover data with PA network, elevation, and SSB grid data across Norway

--- scripts/2.5_corine_PA_elevation_SSB_occurrence_pixels.R
+++ scripts/2.5_corine_PA_elevation_SSB_occurrence_pixels.R
@@ -15,6 +15,7 @@ library(wdpar)
 library(terra)
 library(sf)
 library(tidyverse)
+library(dplyr)
 
 # 1. LOAD DATA ----
 
@@ -23,16 +24,26 @@ library(tidyverse)
 #Add download links
 norway_corine <- ("https://ntnu.box.com/shared/static/ugwtizqcr3a4t4vgxj6qu0rh0h54rtoh.tif")
 ruter500m_Norge <- ("https://ntnu.box.com/shared/static/p8896x2epq4bcmfhorsb5qn2m8mxo5ko.zip")
+dtm50 <- ("https://ntnu.box.com/shared/static/zqrn2zi85d7nm7aaseuvsetazi4qn5uy.zip")
 
 #Download the file to file
+ #land cover
 download.file(norway_corine, here("data",
                                   "corine_modified_classes_stack.tif"))
 
-download.file(ruter500m_Norge, here("data",
+ #SSB grids
+download.file(ruter500m_Norge, here("data", "raw_data",
                                      "ruter500m_Norge.zip"))
 
+ #elevation
+download.file(dtm50, here("data", "raw_data",
+                          "dtm50.zip"))
 #Unzip files
+ #SSB grids
+unzip("ruter500m_Norge.zip")
 
+ #elevation
+unzip("dtm50.zip")
 
 ## 1.2. Download PA network from WDAP database ----
 nor_raw_pa_data <- wdpa_fetch("Norway",
@@ -43,7 +54,12 @@ nor_raw_pa_data <- wdpa_fetch("Norway",
 norway_corine <- rast(here("data",
                                   "corine_modified_classes_stack.tif"))
 
+ssb_grids <- vect(here("data", "raw_data",
+                              "ruter500m_Norge.shp"))
 
+elevation <- vect(here("data", "raw_data",
+                              "dtm50", "metadata",
+                              "dtm50_Tileinndeling.shp"))
 
 #2. PREPARE THE PA, ELEVATION, AND SSB GRID DATA ----
 #Prepare = clean, cut, reproject to match CORINE and transform in the correct object
@@ -96,58 +112,105 @@ plot(nor_pa_terrestrial)
 
 
 #Convert sf object to vector
-norway_PA_vect <- vect(nor_pa_terrestrial,
-                    crs = "+proj=longlat")
+norway_PA_vect <- vect(nor_pa_terrestrial)
 
 #Project norway_PA to same projection as corine_norway
 norway_PA <- terra::project(norway_PA_vect,
                             "epsg:3035")
 
+## 2.2. SSB Grid Data ----
+#Reproject to match corine projection and extent
+norway_ssb_grids <- terra::project(ssb_grids,
+                                   "epsg:3035")
+
+#Check the projection worked as expected
+#projection
+crs(norway_ssb_grids, proj = TRUE) #+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs
+ext(norway_ssb_grids) #SpatExtent : 3999402.08962045, 5135027.9894255, 3851891.81614406, 5439794.32227839 (xmin, xmax, ymin, ymax)
+plot(norway_ssb_grids)
+
+## 2.3. Elevation ----
+#Reproject to match corine projection and extent
+norway_elevation <- terra::project(elevation,
+                                   "epsg:3035")
+
+#Check the projection worked as expected
+#projection
+crs(norway_elevation, proj = TRUE) #+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs
+ext(norway_elevation) #SpatExtent : 3973848.11387648, 5161150.18175419, 3815190.99674517, 5483289.51043574 (xmin, xmax, ymin, ymax)
+plot(norway_elevation)
+
 #3. COMBINE LAND COVER + PA + ELVATION + SSB GRIDS ----
 
 ##3.1. PA Network ----
 
-#Rasterize PA areas to corine_norway
+#Rasterize PA areas to norway_corine
 PA_raster <- terra::rasterize(norway_PA,
-                              corine_norway[[4]])
+                              norway_corine[[4]])
+##3.2. SSB Grids ----
 
-##3.2. Elevation ----
+#Rasterize SSB grids to norway_corine
+SSB_raster <- terra::rasterize(norway_ssb_grids,
+                               norway_corine[[4]])
 
-##3.3. SSB Grids ----
+##3.3. Elevation ----
+
+#rasterize elevation to norway_corine
+elevation_raster <- terra::rasterize(norway_elevation,
+                                     norway_corine[[4]])
+
 
 ##3.4. Combine all data in a single object ----
 
 #Replace NA values with a specific number (to help with extracting the values)
-corine_norway[[4]][is.na(corine_norway[[4]])] <- -9999
-pa_raster[is.na(pa_raster)] <- -9999
-#elevtion
+norway_corine[[4]][is.na(norway_corine[[4]])] <- -9999
+PA_raster[is.na(PA_raster)] <- -9999
 #ssb grids
+SSB_raster[is.na(SSB_raster)] <- -9999
+#elevation
+elevation_raster[is.na(elevation_raster)] <- -9999
 
 
 #Extract values from CORINE and the PA rasterization output
-land_cover_values <- values(corine_norway[[4]])
-norway_pa_values <- values(pa_raster)
-#elevtion
+land_cover_values <- values(norway_corine[[4]])
+norway_pa_values <- values(PA_raster)
 #ssb grids
+norway_ssb_values <- values(SSB_raster)
+#elevation
+norway_elevation_values <- values(elevation_raster)
 
 #Combine rasters in dataframe
 #only PA for now - add the elevation and SSB data after
-all_pixel_data <- data.frame(x = xFromCol(corine_norway[[4]],
-                                    1:ncell(corine_norway[[4]])),
-                       y = yFromCell(corine_norway[[4]],
-                                     1:ncell(corine_norway[[4]])),
+all_pixel_data <- data.frame(x = xFromCol(norway_corine[[4]],
+                                    1:ncell(norway_corine[[4]])),
+                       y = yFromCell(norway_corine[[4]],
+                                     1:ncell(norway_corine[[4]])),
                        land_cover_value = land_cover_values,
-                       protected_area = norway_pa_values)
+                       protected_area = norway_pa_values,
+                       norway_ssb_values = norway_ssb_values,
+                       norway_elevation_values = norway_elevation_values)
 
 
 ## 3.5. Clean dataframe containing the land cover, PA, elevation and SSB grid data ----
+#Check names of df
+names(all_pixel_data)
+
 #Replace the -9999 value with NA (which is what it was from the begining)
 #maybe I should remove the rows that have NA for land cover completely
 all_pixel_data <- all_pixel_data |>
-  mutate(land_cover_values = case_when(land_cover_values == -9999 ~ NA,
-                                       land_cover_values != -9999 ~ land_cover_values),
-         protected_area = case_when(protected_area == NA ~ "N"
-                                    protected_area != NA ~ "Y"))
+  rename("land_cover" = "U2018_CLC2018_V2020_20u1",
+         "PA_status" = "layer",
+         "SSB_grid" = "layer.1",
+         "elevation_m" = "layer.2") |>
+  mutate(land_cover = na_if(land_cover,
+                            land_cover == -9999),
+         PA_status = case_when(PA_status == NA ~ "N",
+                               PA_status != NA ~ "Y"),
+         SSB_grid = na_if(SSB_grid,
+                          SSB_grid == -9999),
+         elevation_m = na_if(elevation_m,
+                             elevation_m == -9999))
 
-
-
+#Save the dataframe
+write.csv(all_pixel_data,
+          here("data", "combine_pixel_data.csv"))
